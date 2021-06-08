4659304caf4a954f041b50e6c7cd4428879c8d78
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2021-06-08 11:12:46 +0200

    Added Modal animations

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,7 +2,7 @@ import { X } from "react-bootstrap-icons"
 import clsx from "clsx"
 
 import "../styles/Modal.scss"
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 
 export interface ModalProps {
     children: ReactNode
@@ -12,15 +12,25 @@ export interface ModalProps {
 }
 
 export default function Modal({ children, show, toggle, title }: ModalProps) {
+    const [fadeOut, setFadeOut] = useState<boolean>(false)
+
+    const doToggle = () => {
+        setFadeOut(true)
+        setTimeout(() => {
+            setFadeOut(false)
+            toggle()
+        }, 200)
+    }
+
     return (
-        <div className={clsx("modal", { show })} role="dialog" tabIndex={-1}>
-            <div className={clsx("modal-backdrop", { show })} onClick={toggle} />
+        <div className={clsx("modal", { show, fadeOut })} role="dialog" tabIndex={-1}>
+            <div className={clsx("modal-backdrop", { show })} onClick={doToggle} />
 
             <div className="modal-content" role="document">
                 <div className="modal-header d-flex justify-content-between mt-3 mb-1">
                     <h2 className="m-0">{title}</h2>
 
-                    <button className="btn-close" onClick={toggle}><X /></button>
+                    <button className="btn-close" onClick={doToggle}><X /></button>
                 </div>
                 <div className="modal-body body-bg p-3 rounded">
                     {children}
