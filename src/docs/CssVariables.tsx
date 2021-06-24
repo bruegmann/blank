ac1cdae771992859bd3c817892b4be5fc1d6e473
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2021-06-24 09:36:16 +0200

    Added overview of CSS vars

---
+++ src/docs/CssVariables.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+
+export interface CssVariablesProps {
+
+}
+
+export default function CssVariables({ }: CssVariablesProps) {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
+    const [cssVariables, setCssVariables] = useState<string[]>([])
+
+    const initCssVariables = () => {
+        const list = Array.from(document.styleSheets)
+            .filter(
+                sheet =>
+                    sheet.href === null || sheet.href.startsWith(window.location.origin)
+            )
+            .reduce(
+                (acc: any, sheet: any) =>
+                (acc = [
+                    ...acc,
+                    ...Array.from<any>(sheet.cssRules).reduce(
+                        (def: any, rule: any) =>
+                        (def =
+                            rule.selectorText === ":root"
+                                ? [
+                                    ...def,
+                                    ...Array.from(rule.style).filter((name: any) =>
+                                        name.startsWith("--")
+                                    )
+                                ]
+                                : def),
+                        []
+                    )
+                ]),
+                []
+            )
+        setCssVariables([...list])
+    }
+
+    useEffect(() => {
+        initCssVariables()
+        prefersDarkScheme.addEventListener("change", initCssVariables)
+        const mutationObserver = new MutationObserver(initCssVariables)
+
+        // Listens if className on :root changes (.light or .dark)
+        mutationObserver.observe(document.documentElement, { attributes: true })
+
+        return () => {
+            prefersDarkScheme.removeEventListener("change", initCssVariables)
+            mutationObserver.disconnect()
+        }
+    }, [])
+
+    return (
+        <>
+            <h2 className="mt-3">CSS variables</h2>
+
+            {cssVariables.map((cssVar: string) =>
+                <div key={cssVar} className="d-flex py-1 gap-2">
+                    <div><code><strong>{cssVar}</strong>:</code></div>
+                    <div><code>{getComputedStyle(document.documentElement).getPropertyValue(cssVar)}</code></div>
+                </div>
+            )}
+        </>
+    )
+}
