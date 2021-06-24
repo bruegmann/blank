abd6ba5b5e188ed153887626723c5c74563cdb4f
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2021-06-24 10:35:28 +0200

    #1 Color preview

--- src/docs/CssVariables.tsx
+++ src/docs/CssVariables.tsx
@@ -55,10 +55,19 @@ export default function CssVariables({ }: CssVariablesProps) {
         <>
             <h2 className="mt-3">CSS variables</h2>
 
+            <p>
+                All <code>*-tone</code> color variables only include the HSL values (e.g. <code>0deg, 0%, 100%</code>). This way you can easily handle
+                the alpha value yourself like this: <code>hsla(var(--body-color-tone), .5)</code>
+            </p>
+
             <div><code>:root {`{`}</code></div>
             {cssVariables.map((cssVar: string) =>
                 <div key={cssVar} className="ms-3">
                     <code><strong>{cssVar}</strong>: {getComputedStyle(document.documentElement).getPropertyValue(cssVar)}</code>
+                    <div
+                        className="d-inline-block border ms-1 rounded"
+                        style={{ width: ".8em", height: ".8em", backgroundColor: cssVar.includes("-tone") ? `hsl(var(${cssVar}))` : `var(${cssVar})` }}
+                    />
                 </div>
             )}
             <div><code>{`}`}</code></div>
